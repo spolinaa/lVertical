9c1c7d50bc9ccba0868875ddb9fd20d362bd6e16
Podkopaev Anton <podkoav239@gmail.com>
2015-04-13 16:02:35 +0300

    Used mono.

--- src/test/VerticalPrinterTest.fs
+++ src/test/VerticalPrinterTest.fs
@@ -11,9 +11,9 @@ let exprTest s =
   let expr = (&s) |> Expr.Parser.parse () |> List.head |> fst
   exprPrinter expr
 
-[<TestCase ("x := 5; y:= 1 - x)", Result=";\n:=\nx\n5\n:=\ny\n-\n1\nx\n")>]
-[<TestCase ("read(x); write(x + 1))", Result=";\nread\nx\nwrite\n+\nx\n1\n")>]
-[<TestCase ("if (x) {write(x + 1)} else {write(x)})", Result="if\nx\nwrite\n+\nx\n1\nwrite\nx\n")>]
+[<TestCase ("x := 5; y:= 1 - x", Result=";\n:=\nx\n5\n:=\ny\n-\n1\nx\n")>]
+[<TestCase ("read(x); write(x + 1)", Result=";\nread\nx\nwrite\n+\nx\n1\n")>]
+[<TestCase ("if (x) {write(x + 1)} else {write(x)}", Result="if\nx\nwrite\n+\nx\n1\nwrite\nx\n")>]
 let stmtTest s =
   let stmt = (&s) |> Stmt.Parser.parse () |> List.head |> fst
   printer stmt
