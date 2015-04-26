64e06dcfcd0ac460630e9ad17fa9f226fdec1d66
Sokolova Polina <sokolova.polina@mail.ru>
2015-04-26 17:47:28 +0300

    Add previous step button

--- src/gui/Program.fs
+++ src/gui/Program.fs
@@ -1,4 +1,4 @@
-﻿open Stmt.Parser
+open Stmt.Parser
 open CoreParser
 open Interpreter
 open System.Drawing
@@ -21,6 +21,24 @@ let programLabel =
 
 let mutable env     : string -> Option<int> = fun (s : string) -> None
 let mutable program : Option<Stmt.t> = None 
+let mutable state : Stmt.t list = []
+
+let prevStepAction (but : Button) args =
+  match state with 
+  | [] -> but.Enabled <- false
+  | a :: list -> 
+    program <- Some a
+    state <- list
+    if list = [] then but.Enabled <- false
+  programLabel.Text <- sprintf "%A" program
+
+let prevStepButton =
+  let but = new Button()
+  but.Text     <- "Previous Step"
+  but.Location <- System.Drawing.Point(programInput.Width - 2 * but.Width - 10, programInput.Height)
+  but.Enabled  <- false
+  but.Click.Add (prevStepAction but)
+  but
 
 let nextStepAction (but : Button) args =
   match program with 
@@ -28,8 +46,11 @@ let nextStepAction (but : Button) args =
   | Some p ->
     let (nenv, np) = ss env p
     env     <- nenv
+    state   <- p :: state
     program <- np
+    if program = None then but.Enabled <- false
     programLabel.Text <- sprintf "%A" program
+    prevStepButton.Enabled <- true
 
 let nextStepButton =
   let but = new Button()
@@ -37,6 +58,7 @@ let nextStepButton =
   but.Location <- System.Drawing.Point(programInput.Width - but.Width, programInput.Height)
   but.Enabled  <- false
   but.Click.Add (nextStepAction but)
+  prevStepButton.Click.Add (fun f -> but.Enabled <- true)
   but
 
 let interpretAction args =
@@ -60,6 +82,7 @@ let mainForm =
   let form = new Form(Visible = false, TopMost = true)
   form.Controls.Add(interpretButton)
   form.Controls.Add(nextStepButton)
+  form.Controls.Add(prevStepButton)
   form.Controls.Add(programInput)
   form.Controls.Add(programLabel)
   form
